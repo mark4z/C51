8496095b9a2e9cff0cb378136a127020268f4c7f
Anymre <anymre@github.com>
2019-03-17 20:09:12 +0800

    1.0.1

--- test/test.c
+++ test/test.c
@@ -1,21 +1,34 @@
 #include<reg51.h>
-
+sbit A1=P1^1;
+sbit B1=P1^0;
 void delay()
 {
     unsigned int i=0,j=0;
-    for(i=0; i<10; i++)
+    for(i=0; i<1; i++)
         for(j=0; j<21738; j++);
+}
+void delay2()
+{
+    unsigned int i=0,j=0;
+    for(i=0; i<1; i++)
+        for(j=0; j<217; j++);
 }
 void main()
 {
+	  char code table[]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90};
     unsigned int i ;
-    unsigned char LED_Data;
+    int shi,ge;
     while(1) {
-        LED_Data=0x01;
-        for(i=0; i<8; i++) {
-            P3=~LED_Data;
-            delay();
-            LED_Data<<=1;
-        }
+			for (i=60;i>0;i--){
+				shi=i/10;
+				ge=i%10;
+				A1=1;B1=0;
+        P3=table[ge];
+				delay2();
+				A1=0;B1=1;
+        P3=table[shi];
+				delay2();
+				delay();
+}
     }
 }
