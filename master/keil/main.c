c0ef7d2f61e44fad1e6ab076fcb65992e3ebf2a0
Anymre <anymre@github.com>
2019-03-20 20:49:51 +0800

    信号灯和倒计时完美

--- master/keil/main.c
+++ master/keil/main.c
@@ -2,20 +2,18 @@
 
 
 //倒计时
-unsigned char buf[4];							//秒显示的变量
-unsigned int EW_time_default = 25;  	  //东西默认值
-unsigned int SN_time_default = 30;			//南北默认值
-unsigned int EW_time_now = 25;		//东西方向当前数秒
-unsigned int SN_time_now = 30;		//南北方向当前数秒
+unsigned char buf[4];							//片选数码管变量
+unsigned int EW_time_default = 10;  	  //东西默认值
+unsigned int SN_time_default = 10;			//南北默认值
+unsigned int EW_time_now = 10;		//东西方向当前数秒
+unsigned int SN_time_now = 10;		//南北方向当前数秒
 
 //通行
 unsigned int SN_or_EW=0; //0：南北通行 1：东西通行
+unsigned int Open=1;
 
 //字型码
 unsigned char code LED[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
-
-
-unsigned char code NUM[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x40}; 
 
 
 //计数
@@ -35,6 +33,8 @@ void delay(int ms);			//延时
 void display();				//显示倒计时
 void init();   				//开机初始化
 void Timer0Init();       //定时器0
+void traffic_light(); //控制信号灯
+void LED_light();      //控制倒计时
 
 void main()
 {
@@ -59,25 +59,15 @@ void time0() interrupt 1
     //重置定时器
     TH0 = 0x4c;
     TL0 = 0x00;
-	  time0_count++;
-    if(time0_count < 20) {}
-    else {
-        time0_count = 0;
-        if(SN_time_now > 0) {
-            SN_time_now--;
-        } else {
-            SN_time_now = EW_time_default;
-        }
-        if(EW_time_now > 0) {
-            EW_time_now--;
-        } else {
-            EW_time_now = EW_time_default;
-        }
-    }
+		//调整信号灯
+		traffic_light();
+	  //调整倒计时
+		LED_light();
 }
 
 void display() //显示子程序
 {
+
 	    
 	
     //南北方向个位十位
@@ -103,6 +93,49 @@ void display() //显示子程序
     P1 = 0X08;				//片选LED4
     P0 = LED[buf[3]];
     delay(1);	
+}
+
+void traffic_light() //信号灯
+{
+	P2=0x00;           //
+	if((SN_or_EW))
+	{
+		//东西方向通行  绿灯亮
+    Green_EW=Open;
+		Red_EW=!Open;
+		Yellow_EW=!Open;
+		//南北方向进禁行 红灯亮
+		Green_SN=!Open;
+		Red_SN=Open;
+		Yellow_SN=!Open;
+  }else{
+		//
+		//东西方向禁行  红灯亮
+    Green_EW=!Open;
+		Red_EW=Open;
+		Yellow_EW=!Open;
+		//南北方向通行绿灯亮
+		Green_SN=Open;
+		Red_SN=!Open;
+		Yellow_SN=!Open;
+}
+}
+
+void LED_light() //倒计时
+{
+		time0_count++;
+    if(time0_count < 20) {}    //每秒检测一次，归零后重置，并切换通信方向
+    else {
+        time0_count = 0;
+        if(SN_time_now > 0) {
+            SN_time_now--;
+					  EW_time_now--;
+        } else {
+            SN_time_now = SN_time_default;
+					  EW_time_now = EW_time_default;
+						SN_or_EW=!SN_or_EW;
+        }
+    }
 }
 
 void delay(int ms)			//延时子程序
